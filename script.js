921f492943b85f80fea190f5f938d32850a2d4b0
Szabó Dávid <20d_szabod@NYIRSZIKSZI.local>
2023-03-28 09:40:55 +0200

    Apró változtatások, képeken szövegen.

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ const ubuntu_btn = document.getElementById("ubuntu_btn");
 const ubuntu_mate_bnt = document.getElementById("ubuntu_mate_btn");
 const windows_iot_btn = document.getElementById("windows_iot_btn");
 
+
+const img = document.querySelector('img');
+img.ondragstart = () => {
+    return false;
+};
+
+
+
 // event listenerek a gombokra
 // ubuntu
 ubuntu_btn.addEventListener("click", () => {
