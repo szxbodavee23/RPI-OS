9dcec8cf823886401fdeaba731077d7fdec1c105
Szántó Dávid <20d_szantod@NYIRSZIKSZI.local>
2023-04-04 11:04:20 +0200

    Menu gombok befejezve

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ const ubuntu_mate_szoveg = "Felhasználóbarát felület: Az Ubuntu MATE felhasz
 const windows_szoveg = "Jól ismert környezet: A Windows IoT operációs rendszer jól ismert és elterjedt az iparban, ezért sok fejlesztő számára ismerős lehet. Ez azt jelenti, hogy az átállás könnyebb lehet a fejlesztőknek, akik már tapasztalattal rendelkeznek a Windows fejlesztés terén. Felhő integráció: A Windows IoT operációs rendszer lehetővé teszi a felhő alapú szolgáltatásokkal való integrációt, mint például az Azure IoT Hub, a Microsoft felhőalapú IoT platformja. Ez lehetővé teszi a felhasználók számára az eszközeik távoli felügyeletét és irányítását. Windows kompatibilitás: A Windows IoT operációs rendszer lehetővé teszi a Windows-kompatibilis alkalmazások futtatását a Raspberry Pi-n. Ez azt jelenti, hogy a fejlesztőknek nem kell külön alkalmazásokat fejleszteniük a Raspberry Pi-hoz.";
 
 back.addEventListener("click", () => {
-    // szoveg valtoztatasa
+    // szoveg vissza valtoztatasa
     ubuntu_btn.innerHTML = ubuntu_txt;
     ubuntu_mate_bnt.innerHTML = ubuntu_mate_txt;
     windows_iot_btn.innerHTML = windows_txt;
@@ -46,12 +46,15 @@ img.ondragstart = () => {
     return false;
 };
 
-
+const $ = (id) => {
+    return document.getElementById(id);
+}
 
 // event listenerek a gombokra
 // ubuntu
 ubuntu_btn.addEventListener("click", () => {
     if (ubuntu_btn.innerHTML == ubuntu_txt) {
+        // elonyok hatranyok
         mid.innerHTML = ubuntu_txt;
         ubuntu_btn.innerHTML = "Előnyök";
         windows_iot_btn.innerHTML = "Hátrányok";
@@ -59,14 +62,41 @@ ubuntu_btn.addEventListener("click", () => {
         texts.innerHTML = "Ha ismered az Ubuntu asztali környezetet a számítógépeden vagy laptopodon, akkor azt kell mondanom, hogy semmi sem változik, amikor az Ubuntu-t futtatod a Raspberry Pi-n. Valójában, ha főként otthoni irodai munkát végzel (dokumentumok írása, internetezés, videók nézése stb.), akkor akár a számítógépedet is kicserélheted egy Raspberry Pi-re, amely sok pénzt takaríthat meg az energia számládon. Az Ubuntu 20.10 azonos alkalmazásokat tartalmaz, mint az Ubuntu asztali környezete GNOME-alapokon, és még egy kis pluszt is kap, mivel a Raspberry Pi-hoz készült Ubuntu 20.10 egyedi válogatással rendelkezik az alapértelmezett háttérképek között, amelyek máshol nincsenek.";
     }
     else {
-        texts.innerHTML = mid.innerHTML + " Előnyök";
+        texts.innerHTML = "";
 
-        text_containers[0].style = "display: block;";
+        if (mid.innerHTML == ubuntu_txt) {
+            $("ubuntu_elonyok").style = "display: block";
+            $("ubuntu_hatranyok").style = "display: none;";
+        }
+        else if (mid.innerHTML == ubuntu_mate_txt) {
+            $("ubuntu_mate_elonyok").style = "display: block";
+            $("ubuntu_mate_hatranyok").style = "display: none;";
+        }
+        else if (mid.innerHTML == windows_txt) {
+            $("windows_elonyok").style = "display: block";
+            $("windows_hatranyok").style = "display: none;";
+        }
     }
 });
 
 // ubuntu mate
 ubuntu_mate_bnt.addEventListener("click", () => {
+    if (mid.innerHTML == ubuntu_txt) {
+        text_containers[0].style = "display: none;";
+        text_containers[3].style = "display: none;";
+        texts.innerHTML = "Ha ismered az Ubuntu asztali környezetet a számítógépeden vagy laptopodon, akkor azt kell mondanom, hogy semmi sem változik, amikor az Ubuntu-t futtatod a Raspberry Pi-n. Valójában, ha főként otthoni irodai munkát végzel (dokumentumok írása, internetezés, videók nézése stb.), akkor akár a számítógépedet is kicserélheted egy Raspberry Pi-re, amely sok pénzt takaríthat meg az energia számládon. Az Ubuntu 20.10 azonos alkalmazásokat tartalmaz, mint az Ubuntu asztali környezete GNOME-alapokon, és még egy kis pluszt is kap, mivel a Raspberry Pi-hoz készült Ubuntu 20.10 egyedi válogatással rendelkezik az alapértelmezett háttérképek között, amelyek máshol nincsenek.";
+    }
+    else if (mid.innerHTML == ubuntu_mate_txt) {
+        text_containers[1].style = "display: none";
+        text_containers[4].style = "display: none;";
+        texts.innerHTML = "A Raspberry Pi egyike a legnépszerűbb számítógép platformoknak, amelyen sokféle operációs rendszer futtatható, köztük az Ubuntu MATE is. Az Ubuntu MATE egy könnyen használható és felhasználóbarát operációs rendszer, amely kifejezetten a Raspberry Pi és más ARM-alapú számítógépek számára készült.";
+    }
+    else {
+        text_containers[2].style = "display: none;";
+        text_containers[5].style = "display: none;";
+        texts.innerHTML = "A Raspberry Pi egy kis méretű számítógép, amelyet általában a Linux alapú Raspbian operációs rendszerrel használnak. Azonban a Windows IoT (Internet of Things) operációs rendszer is támogatja a Raspberry Pi-t, és lehetővé teszi a számítógép alkalmazásait és eszközeit az IoT-rendszerbe való integrálását.";
+    }
+
     if (ubuntu_mate_bnt.innerHTML == ubuntu_mate_txt) {
         mid.innerHTML = ubuntu_mate_txt;
         ubuntu_btn.innerHTML = "Előnyök";
@@ -86,7 +116,20 @@ windows_iot_btn.addEventListener("click", () => {
         texts.innerHTML = "A Raspberry Pi egy kis méretű számítógép, amelyet általában a Linux alapú Raspbian operációs rendszerrel használnak. Azonban a Windows IoT (Internet of Things) operációs rendszer is támogatja a Raspberry Pi-t, és lehetővé teszi a számítógép alkalmazásait és eszközeit az IoT-rendszerbe való integrálását.";
     }
     else {
-        texts.innerHTML = mid.innerHTML + " Hátrányok";
+        texts.innerHTML = "";
+
+        if (mid.innerHTML == ubuntu_txt) {
+            $("ubuntu_hatranyok").style = "display: block";
+            $("ubuntu_elonyok").style = "display: none";
+        }
+        else if (mid.innerHTML == ubuntu_mate_txt) {
+            $("ubuntu_mate_hatranyok").style = "display: block";
+            $("ubuntu_mate_elonyok").style = "display: none";
+        }
+        else if (mid.innerHTML == windows_txt) {
+            $("windows_hatranyok").style = "display: block";
+            $("windows_elonyok").style = "display: none";
+        }
     }
 });
 
